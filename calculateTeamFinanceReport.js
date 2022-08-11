ce4ba5ba992c91f43b5e2b9dc98782fdb4380913
Vova <strikerxp1990@gmail.com>
2022-08-11 13:47:30 +0300

    New formula

--- calculateTeamFinanceReport.js
+++ calculateTeamFinanceReport.js
@@ -3,12 +3,9 @@ const salaries = {
         salary: 1000, // salary after tax; should be integer; min: 100, max: 100000
         tax: '15%' // tax percent; presented as string with template `{tax}%` where 'tax' is integer; min: "0%", max: "99%"
     },
-    Tester: {
-        salary: 800,
-        tax: '10%'
-    },
-    Designer: { salary: 600, tax: '30%' },
-    Artist: { salary: 1500, tax: '15%' }
+    Tester: {salary: 800, tax: '10%'},
+    Designer: {salary: 600, tax: '30%'},
+    Artist: {salary: 1500, tax: '15%'}
 };
 
 const team = [
@@ -16,37 +13,34 @@ const team = [
         name: 'Masha', // name of team member
         specialization: 'Progger' // specialization should be picked from `salaries` otherwise member should be ignored in report
     },
-    { name: 'Vasya', specialization: 'Tester' },
-    { name: 'Taras', specialization: 'Tester' },
-    { name: 'Taras I', specialization: 'Tester' },
-    { name: 'Tapas', specialization: 'Humor' },
-    { name: 'Misha', specialization: 'Manager' },
-    { name: 'Max', specialization: 'Designer' },
-    { name: 'Vova', specialization: 'Designer' },
-    { name: 'Leo', specialization: 'Artist' }
+    {name: 'Vasya', specialization: 'Tester'},
+    {name: 'Taras', specialization: 'Tester'},
+    {name: 'Taras I', specialization: 'Tester'},
+    {name: 'Tapas', specialization: 'Humor'},
+    {name: 'Misha', specialization: 'Manager'},
+    {name: 'Max', specialization: 'Designer'},
+    {name: 'Vova', specialization: 'Designer'},
+    {name: 'Leo', specialization: 'Artist'}
 ];
 
 function calculateTeamFinanceReport(salaries, team) {
     const proffNames = Object.keys(salaries);//?
     let result = {};
-    let totalResult = {};
-    let totalBudgetTeam = 0;
+    result['totalBudgetTeam'] = 0;
 
-    for (let arrayElement of team) {
-        if (proffNames.includes(arrayElement.specialization))
-            result[arrayElement.specialization] = (result[arrayElement.specialization] || 0) + 1;
-    }
+    team.filter(fromSalaries => proffNames.includes(fromSalaries.specialization))
+        .map(arrayElement => {
+            let specialization = arrayElement.specialization
+            if (proffNames.includes(specialization)) {
+                let formula = Object.values(salaries[specialization]).reduce((prev, acc) => prev + (prev * parseInt(acc)) / 100);
+                result[specialization] = (result[specialization] || 0) + formula;
+                result['totalBudgetTeam'] += result[specialization]//?
+            }
+        })
 
-    totalResult['totalBudgetTeam'] = 0;
+    result//?
 
-    Object.entries(result).forEach(([item, salary]) => {//?
-        const eachSalary = salaries[item]['salary'];
-        totalBudgetTeam += totalResult[`totalBudget${item}`] = (eachSalary + (eachSalary * parseInt(salaries[item]['tax'])) / 100) * salary;//?
-    });//?
-
-    totalResult['totalBudgetTeam'] = totalBudgetTeam;
-
-    return totalResult//?
+    return result//?
 }
 
 console.log(JSON.stringify(calculateTeamFinanceReport(salaries, team)))
