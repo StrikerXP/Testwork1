c1dcaa64129e1baf08268c5e915183314d8bf6e2
StrikerXP <105562236+StrikerXP@users.noreply.github.com>
2023-01-23 12:09:12 +0200

    Buf fixes
    Bug fixing in main code

--- calculateTeamFinanceReport.js
+++ calculateTeamFinanceReport.js
@@ -1,48 +1,58 @@
-const salaries = {
-    Progger: { // specialization type 'Progger'
-        salary: 1000, // salary after tax; should be integer; min: 100, max: 100000
-        tax: '15%' // tax percent; presented as string with template `{tax}%` where 'tax' is integer; min: "0%", max: "99%"
-    },
-    Tester: {salary: 800, tax: '10%'},
-    Designer: {salary: 600, tax: '30%'},
-    Artist: {salary: 1500, tax: '15.9%'}
-};
+const salaries1 = {
+    Manager: {salary: 1000, tax: "10%"},
+    Designer: {salary: 600, tax: "30%"},
+    Artist: {salary: 1500, tax: "15%"},
+}
 
-const team = [
-    {
-        name: 'Masha', // name of team member
-        specialization: 'Progger' // specialization should be picked from `salaries` otherwise member should be ignored in report
-    },
-    {name: 'Vasya', specialization: 'Tester'},
-    {name: 'Taras', specialization: 'Tester'},
-    {name: 'Taras I', specialization: 'Tester'},
-    {name: 'Tapas', specialization: 'Humor'},
-    {name: 'Misha', specialization: 'Manager'},
-    {name: 'Max', specialization: 'Designer'},
-    {name: 'Vova', specialization: 'Designer'},
-    {name: 'Leo', specialization: 'Artist'}
-];
+const team1 = [
+    {name: "Misha", specialization: "Manager"},
+    {name: "Max", specialization: "Designer"},
+    {name: "Vova", specialization: "Designer"},
+    {name: "Leo", specialization: "Artist"},]
+
+
+const salaries2 = {
+    TeamLead: {salary: 1000, tax: "99%"},
+    Architect: {salary: 9000, tax: "34%"},
+}
+
+const team2 = [
+    {name: "Alexander", specialization: "TeamLead"},
+    {name: "Gaudi", specialization: "Architect"},
+    {name: "Koolhas", specialization: "Architect"},
+    {name: "Foster", specialization: "Architect"},
+    {name: "Napoleon", specialization: "General"},
+]
+
 
 function calculateTeamFinanceReport(salaries, team) {
+    // get 'name' salaries report
     const proffNames = Object.keys(salaries);
+    // final result variable
     let result = {};
+    // variable for 'totalBudgetTeam'
     result['totalBudgetTeam'] = 0;
 
-    team.filter(fromSalaries => proffNames.includes(fromSalaries.specialization))
+    // filter team members that are present in 'team'
+    team.filter(specializationName => proffNames.includes(specializationName.specialization))//?
         .forEach(arrayElement => {
-            let specialization = arrayElement.specialization
-
-            if (proffNames.includes(specialization)) {
-                let formula = Object.values(salaries[specialization])
-                    .reduce((prev, acc) =>
-                        //prev = salary as integer; acc = 'tax' is integer;
-                        prev + (prev * parseInt(acc)) / 100);
-                result[`totalBudget${specialization}`] = (result[`totalBudget${specialization}`] || 0) + formula;
-                result['totalBudgetTeam'] += result[`totalBudget${specialization}`];
+                // get specialization name
+                let specialization = arrayElement.specialization
+                Object.values(salaries[specialization])
+                    // calculating report
+                    .reduce((prev, acc) => {
+                        // prev = 'salary' as integer; acc = 'tax' is integer;
+                        let formula = prev + (prev * parseInt(acc)) / 100;
+                        result[`totalBudget${specialization}`] = (result[`totalBudget${specialization}`] || 0) + formula;//?
+                        result['totalBudgetTeam'] += formula;
+                    })
             }
-        })
-
+        )
     return result
 }
 
-console.log(JSON.stringify(calculateTeamFinanceReport(salaries, team)))
+const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
+const financeReport2 = calculateTeamFinanceReport(salaries2, team2)
+
+console.log(JSON.stringify(financeReport1))
+console.log(JSON.stringify(financeReport2))
